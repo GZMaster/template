7763d86a42801f3165bdc516d85eb2a8420c522f
GZMaster <33158650+GZMaster@users.noreply.github.com>
2024-01-10 13:12:58 +0100

    Add new tasks and update solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,11 @@
+import "./tasks/stake";
+import "./tasks/claim";
+import "./tasks/unstake";
+import "./tasks/beneficiary";
+import "./tasks/withdraw";
+import "./tasks/unstake";
+import "./tasks/address";
+import "./tasks/deploy";
 import "@nomicfoundation/hardhat-toolbox";
 import "@zetachain/toolkit/tasks";
 
@@ -5,10 +13,10 @@ import { getHardhatConfigNetworks } from "@zetachain/networks";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  networks: {
-    ...getHardhatConfigNetworks(),
-  },
-  solidity: "0.8.7",
+	networks: {
+		...getHardhatConfigNetworks(),
+	},
+	solidity: "0.8.7",
 };
 
 export default config;
